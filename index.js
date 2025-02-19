be8b41a72405e50a42f51a0b49ce946efabae141
zohaibhassan9 <zabaidafashion@gmail.com>
2025-02-19 20:25:38 +0500

    JS code completed , and nowApp running smootly

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const length = document.getElementById("length").value;
-
 function generatePassword(){
-
-
-}
-
-function copyPassword(){
+const length = document.getElementById("length").value;
 let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456!@#$%^";
 let password = '';
 
 
 for (let i=0; i< length; i++){
-let randomIndex = Math.floor(Math.random()*length.characters);
+let randomIndex = Math.floor(Math.random() *characters.length);
 password += characters[randomIndex];
 }
 
+document.getElementById('password').value = password;
+}
+
+function copyPassword(){
+let passwordField = document.getElementById('password');
+passwordField.select();
+document.execCommand('copy');
+
+alert('Password Coopied!')
 }
